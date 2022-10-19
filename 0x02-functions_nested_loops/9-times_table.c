dcb23a90af5f1a500b2d9c482f06294c00b400b5
Khanyi Gina <khanyigina37@gmail.com>
2022-10-19 07:28:04 -0700

    added task nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,17 +22,17 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				-putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
-				_putchar(',')
-				_putchar(' ')
+				_putchar(',');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
 		}
-		-putchar('\n');
+		_putchar('\n');
 	}
 }
