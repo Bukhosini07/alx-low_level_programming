5fb734e5bb0e3296de1f6fa9ea3f98578ab066f6
KhanyiginaSA <khanyigina37@gmail.com>
2022-11-10 04:47:24 -0800

    added all files

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - write the character c to stdout
  * @c: char to be printed
  * Return: 1 on success
  */
 
 int _putchar(char c)
 {
-	return (writes(1, &c, 1));
+	return (write(1, &c, 1));
 }
