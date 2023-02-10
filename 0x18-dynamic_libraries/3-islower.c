cedce610fd46ffb49998b8faccd4cdc0405403ec
KhanyiginaSA <khanyigina37@gmail.com>
2023-02-10 00:32:52 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c lowercase, 0 otherwise
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
  */
 
 int _islower(int c)
